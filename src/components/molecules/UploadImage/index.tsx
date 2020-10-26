edf22b18eb3fea7e0573a727139557e9e8b9431d
mickey <lemanhhungb01501@gmail.com>
2020-10-26 13:57:41 +0700

    add IconImage

---
+++ src/components/molecules/UploadImage/index.tsx
@@ -0,0 +1,60 @@
+// import node_modules
+import React, {useRef} from "react";
+import styled from "styled-components";
+
+// import components
+import {Text} from "../../atoms/Text";
+import {IconImage} from "../../atoms/IconImage";
+import {Colors} from "../../../styles/Colors";
+
+type UploadImageProps = {
+  onUpload: (file: File) => void
+}
+
+export const UploadImage = ({onUpload}: UploadImageProps) => {
+  const inputFileRef = useRef<HTMLInputElement>(null)
+
+  const handleUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault()
+    if (!event.target?.files) return
+    const file = event.target.files[0]
+    onUpload(file)
+  }
+
+  const handleClick = () => {
+    if (!inputFileRef || !inputFileRef.current) return
+    inputFileRef.current.click()
+  }
+
+  return <ContentWrapper onClick={handleClick}>
+    <Text colorType={"white"} size={"small"}>upload image</Text>
+    <ImageWrapper>
+      <IconImage />
+    </ImageWrapper>
+    <InputFileHidden
+      ref={inputFileRef}
+      type="file"
+      name="file"
+      accept="image/x-png,image/jpeg"
+      onChange={handleUpload}
+    />
+  </ContentWrapper>
+
+}
+
+const ContentWrapper = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  color: ${Colors.COLOR_FFFFFF};
+  cursor: pointer;
+`
+const ImageWrapper = styled.span`
+  width: 10px;
+  padding-left: 5px;
+  height: auto;
+`
+
+const InputFileHidden = styled.input`
+  display: none;
+`
