d7e33715aba8875423c2aeb19de39161931a70fa
mickey <lemanhhungb01501@gmail.com>
2020-10-26 13:09:51 +0700

    update colors upload image

--- src/components/molecules/Markdown/index.tsx
+++ src/components/molecules/Markdown/index.tsx
@@ -1,13 +1,18 @@
+// import node_modules
 import React from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import atomOneDark from 'react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark';
 import ReactMarkdown from "react-markdown";
 
+// import others
+import {Colors} from "../../../styles/Colors";
+
 type MarkdownProps = {
   source: string,
 }
 
-atomOneDark.hljs.background = "#0c1020"
+atomOneDark.hljs.background = Colors.COLOR_0C1020
+atomOneDark.hljs.color = Colors.COLOR_FFFFFF
 atomOneDark.hljs.fontSize = 13
 
 export const Markdown = ({source}: MarkdownProps) => {
